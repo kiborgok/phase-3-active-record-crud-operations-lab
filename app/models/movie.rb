28e60c54d63e62c80690f03bc1ae7b8dd90d1b61
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-17 11:31:26 +0300

    commit tree 41cbc2520ebc00ad3f63444987caaf6fc0cc7aa9

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ActiveRecord::Base
-
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+    end
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
 end
